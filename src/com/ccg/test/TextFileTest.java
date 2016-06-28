33a5edee3314b9f448a07aa516b04b6d6a81b771
cuichenguang <2631426315@qq.com>
2016-06-28 16:38:13 +0800

    文本文件操作工具类

---
+++ src/com/ccg/test/TextFileTest.java
@@ -0,0 +1,17 @@
+package com.ccg.test;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.ccg.util.FileUtil;
+
+public class TextFileTest {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		/*String charset = "UTF-8";
+		FileUtil.readTxtFile("d:/ccg.txt",charset);*/
+		System.out.println(System.getProperty("line.separator"));
+		
+	}
+}
